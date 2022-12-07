ed680d3e493bd8eb93ba3ff92abc1d24539a8fd3
swiftsolves-msft <30509195+swiftsolves-msft@users.noreply.github.com>
2022-12-07 16:57:58 -0500

    Add files via upload

---
+++ AzureToMDEDeviceTag/MDEDeviceTag.ps1
@@ -0,0 +1,16 @@
+﻿configuration MDEDeviceTag
+{
+    Import-DscResource -ModuleName PSDscResources
+    Node localhost
+    {
+        Registry MDEDeviceTag
+        {
+            Key = 'HKLM:\SOFTWARE\Policies\Microsoft\Windows Advanced Threat Protection\DeviceTagging'
+            Ensure = 'Present'
+            ValueName = 'Group'
+            ValueType = 'String'
+            ValueData = 'MDCOnboardAzure'
+            Force = $true
+        }
+    }
+}
